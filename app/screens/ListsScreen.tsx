6abe4cbc1bf04716b2e24ed7a55f21a97185fa6c
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2022-11-28 19:52:49 +0100

    wip: ListsScreen styling

--- app/screens/ListsScreen.tsx
+++ app/screens/ListsScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { Dimensions, TouchableOpacity } from 'react-native';
 import styled from '@emotion/native';
 
 import { Text, View } from 'components/Themed';
@@ -7,13 +7,22 @@ import PercolateIcon from 'constants/Percolate';
 import { useAppData } from 'providers/DataProvider';
 import { type RootTabScreenProps } from 'types/navigation';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
+const OuterContainer = styled(View)`
+  flex: 1;
+  padding: 16px;
+`;
+
+const ListsHeaderContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 16px;
+`;
+
+const HeaderText = styled.Text`
+  font-size: 16px;
+  text-transform: uppercase;
+`;
 
 const AddListButton = styled(TouchableOpacity)`
   background-color: mediumturquoise;
@@ -22,9 +31,6 @@ const AddListButton = styled(TouchableOpacity)`
   justify-content: center;
   height: 32px;
   width: 32px;
-  position: absolute;
-  top: 20px;
-  right: 20px;
 `;
 
 const StyledList = styled(TouchableOpacity)`
@@ -43,30 +49,34 @@ export function ListsScreen({ navigation }: RootTabScreenProps<'ListsScreen'>) {
   // console.log('ListsScreen: taskLists recevied from DataContext:', taskLists);
 
   return (
-    <View style={styles.container}>
+    <OuterContainer>
+      <ListsHeaderContainer>
+        <HeaderText>
+          My lists
+        </HeaderText>
+        <AddListButton
+          onPress={() => navigation.navigate('CreateNewListScreen')}
+        >
+          <PercolateIcon
+            color={'white'}
+            name="plus"
+            size={16}
+          />
+        </AddListButton>
+      </ListsHeaderContainer>
+
       {Object.values(taskLists).map((list) => {
         return (
-          <>
-            <AddListButton
-              onPress={() => navigation.navigate('CreateNewListScreen')}
-            >
-              <PercolateIcon
-                color={'white'}
-                name="plus"
-                size={16}
-              />
-            </AddListButton>
-            <StyledList
-              key={list.id}
-              onPress={() => navigation.navigate('TasksScreen', { listId: list.id })}
-            >
-              <Text>
-                {list.name}
-              </Text>
-            </StyledList>
-          </>
+          <StyledList
+            key={list.id}
+            onPress={() => navigation.navigate('TasksScreen', { listId: list.id })}
+          >
+            <Text>
+              {list.name}
+            </Text>
+          </StyledList>
         )
       })}
-    </View>
+    </OuterContainer>
   );
 }
