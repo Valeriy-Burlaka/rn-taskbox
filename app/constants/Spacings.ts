486c01861852fb725282bf0dd3db99c6c4cfe0c5
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2022-12-02 13:26:12 +0100

    wip: Calculate the size of palette item dynamically; Replace hard-code spacings with constants

---
+++ app/constants/Spacings.ts
@@ -0,0 +1,6 @@
+export const spacings = {
+  space50: '8px',
+  space100: '16px',
+  space200: '32px',
+  space300: '48px',
+};
--- app/screens/CreateNewListScreen.tsx
+++ app/screens/CreateNewListScreen.tsx
@@ -1,66 +1,74 @@
 import { useState } from 'react';
-import { Platform, TextInput } from 'react-native';
+import { Platform } from 'react-native';
+import styled from '@emotion/native';
 import * as Haptics from 'expo-haptics';
 import { StatusBar } from 'expo-status-bar';
 import { rgba } from 'polished';
-import styled from '@emotion/native';
+
+import { spacings } from 'constants/Spacings';
+import { SCREEN_WIDTH } from 'utils/dimensions';
 
 const OuterContainer = styled.View<{ backgroundColor: string }>`
   background-color: ${({ backgroundColor }) => rgba(backgroundColor, 0.2)};
   flex: 1;
-  padding: 16px;
+  padding: ${spacings.space100};
 `;
 
 const StyledTextInput = styled.TextInput`
   background-color: white;
-  border-radius: 8px;
-  height: 48px;
-  margin-bottom: 48px;
+  border-radius: ${spacings.space50};
+  height: ${spacings.space300};
+  margin-bottom: ${spacings.space300};
   text-align: center;
 `;
 
 const PaletteContainer = styled.View`
   background-color: white;
-  border-radius: 8px;
+  border-radius: ${spacings.space50};
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-between;
-  padding: 8px;
+  padding: ${spacings.space50};
 `;
 
-const PaletteItem = styled.TouchableOpacity<{ backgroundColor: string; selected?: boolean }>`
-  background-color: ${({ backgroundColor }) => backgroundColor};
-  border-radius: 100px;
-  height: 40px;
-  width: 40px;
-  margin: 8px;
+const PaletteItem = styled.TouchableOpacity<{
+  backgroundColor: string;
+  size: string;
+  selected?: boolean
+}>`
+    background-color: ${({ backgroundColor }) => backgroundColor};
+    border-radius: 100px;
+    height: ${({ size }) => size};
+    width: ${({ size }) => size};
+    margin: ${spacings.space50};
 
-  /* Not the view I'm looking for.
-     Basically, I want to render a solid border around a selected palette item, not a blurry shadow.
-     Will need to apply some hack, like rendering a wrapper View around.
-  */
-  shadow-color: ${({ selected }) => selected ? 'black' : undefined};
-  shadow-offset: ${({ selected }) => selected ? '0px 0px' : undefined};
-  shadow-opacity: ${({ selected }) => selected ? '1' : undefined};
-  shadow-radius: ${({ selected }) => selected ? '3px' : undefined};
-  /* https://github.com/styled-components/styled-components/issues/709 */
-  /* Elevation is a workaround for Android. Shadow don't work on Android */
-  /* elevation: 0; */
+    /* Not the view I'm looking for.
+      Basically, I want to render a solid border around a selected palette item, not a blurry shadow.
+      Will need to apply some hack, like rendering a wrapper View around.
+    */
+    shadow-color: ${({ selected }) => selected ? 'black' : undefined};
+    shadow-offset: ${({ selected }) => selected ? '0px 0px' : undefined};
+    shadow-opacity: ${({ selected }) => selected ? '1' : undefined};
+    shadow-radius: ${({ selected }) => selected ? '3px' : undefined};
+    /* https://github.com/styled-components/styled-components/issues/709 */
+    /* Elevation is a workaround for Android. Shadow don't work on Android */
+    /* elevation: 0; */
 
-  /* Doesn't allow 4th parameter (spread radius) */
-  /* box-shadow: 0px 0px 10px grey; */
+    /* Doesn't allow 4th parameter (spread radius) */
+    /* box-shadow: 0px 0px 10px grey; */
 
-  /* Doesn't work for me - borders are rendered inside (inward) the View */
-  /* border-color: lightgrey;
-  border-style: dotted;
-  border-width: 5px; */
-`;
+    /* Doesn't work for me - borders are rendered inside (inward) the View */
+    /* border-color: lightgrey;
+    border-style: dotted;
+    border-width: 5px; */
+  `;
 
 // export component for Modal Header (replaces default)
 // cross-button (close modal) -- <Icon name="list" /> -- "Save" button (saves new list; disabled when title is empty)
 // !! Icon is the current one selected by Icon-picker
 
-const palette = [
+// https://www.w3schools.com/tags/ref_colornames.asp
+export const palette = [
   // 1st row
   '#00FA9A', // MediumSpringGreen
   '#F08080', // LightCoral
@@ -88,8 +96,22 @@ export function CreateNewListScreen() {
   const [activeColor, setActiveColor] = useState(palette[0]);
   const [listTitle, setListTitle] = useState('');
 
+  const paletteItemsPerRow = 6;
+  const outerPadding = Number(spacings.space100.replace('px', ''));
+  console.log('Outr spacing:', outerPadding);
+  const paletteItemSize = Math.floor(
+    (
+      SCREEN_WIDTH
+        - (outerPadding * 2)
+        - (outerPadding * (paletteItemsPerRow + 1))
+    )
+      / paletteItemsPerRow
+  );
+  console.log(`Calculated size of palette item for screen width=${SCREEN_WIDTH}px : ${paletteItemSize}px`);
+
   return (
     <OuterContainer backgroundColor={activeColor}>
+      {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
 
       <StyledTextInput
@@ -110,18 +132,13 @@ export function CreateNewListScreen() {
                 setActiveColor(color);
               }}
               selected={color === activeColor}
+              size={`${paletteItemSize}px`}
             />
           );
         })}
       </PaletteContainer>
 
       {/* Icon picker */}
-
-
-      {/* <Text style={styles.title}>Create new list</Text> */}
-      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
     </OuterContainer>
   );
 }
