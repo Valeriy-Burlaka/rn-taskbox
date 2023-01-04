23117d99a38b65a493a8492ea352030a087a37ea
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2023-01-04 13:26:19 +0100

    wip: re-work header for TaskList screen (move to a standalone component)

---
+++ app/components/TaskList/components/Header.tsx
@@ -0,0 +1,71 @@
+import { TouchableOpacity, View } from 'react-native';
+
+import ContextMenu from 'react-native-context-menu-view';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import styled from '@emotion/native';
+
+import { TaskList } from 'types';
+import { spacings } from 'theme/Spacings';
+import { textSizes } from 'theme/Typography';
+
+const HeaderContainer = styled.View<{}>`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  border: 1px solid red;
+  margin-bottom: ${spacings.space200};
+  padding: ${spacings.space100};
+`;
+
+const ListName = styled.Text<{ color: string }>`
+  color: ${({ color }) => color};
+  font-family: NunitoSans-Bold;
+  font-size: ${textSizes.regular};
+`;
+
+export type Props = Pick<TaskList, 'name' | 'color'>
+
+export function Header({ name, color }: Props) {
+  return (
+    <HeaderContainer>
+      {/* Back button */}
+      <TouchableOpacity
+      >
+        <Ionicons name="chevron-back" size={30} color={color} />
+      </TouchableOpacity>
+
+      {/* List name */}
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+        <ListName
+          color={color}
+        >
+          {name}
+        </ListName>
+      </View>
+
+      {/* 3-dots menu */}
+      <ContextMenu
+        actions={[
+          {
+            title: 'Edit List Info',
+            subtitletitle: 'WAT',
+            systemIcon: 'pencil',
+          },
+          {
+            title: 'Delete',
+            subtitletitle: 'WAT?',
+            systemIcon: 'trash',
+            destructive: true,
+          },
+        ]}
+        dropdownMenuMode={true}
+      >
+        <Ionicons
+          color={color}
+          name="ios-ellipsis-horizontal-circle"
+          size={26}
+        />
+      </ContextMenu>
+    </HeaderContainer>
+  );
+}
