7296dced6918ab81980bf1dc0bcae96f1d305c40
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2023-02-25 22:41:39 +0100

    wip: prepare positions

--- app/components/TaskList/components/SortingTaskListView.tsx
+++ app/components/TaskList/components/SortingTaskListView.tsx
@@ -1,4 +1,11 @@
+import { View } from 'components/Themed';
+import { useState } from 'react';
 import { ScrollView } from 'react-native';
+import {
+  useSharedValue,
+  runOnUI,
+  runOnJS,
+} from 'react-native-reanimated';
 
 import { TaskData, TaskStates } from 'types/task';
 
@@ -9,13 +16,71 @@ interface Props {
 }
 
 export function SortingTaskListView({ tasks }: Props) {
-  const sortableTasks = tasks.filter(t => t.state === TaskStates.TASK_INBOX)
+  const [isReady, setReady] = useState(false);
+  console.log(isReady);
+
+  const sortableTasks = tasks.filter(t => t.state === TaskStates.TASK_INBOX);
+
+  const positions = sortableTasks.map((task) => {
+    return {
+      id: task.id,
+      order: useSharedValue(0),
+      originalX: useSharedValue(0),
+      originalY: useSharedValue(0),
+      x: useSharedValue(0),
+      y: useSharedValue(0),
+      height: useSharedValue(0),
+      width: useSharedValue(0),
+      ready: useSharedValue(false),
+    };
+  });
+
+  if (!isReady) {
+    return (
+      <ScrollView>
+        {sortableTasks.map((task, index) => {
+          return (
+            <View
+              key={task.id}
+              onLayout={({
+                nativeEvent: {
+                  layout: { x, y, height, width, },
+                },
+              }) => {
+                const position = positions[index];
+                position.order.value = index;
+                position.originalX.value = x;
+                position.originalY.value = y;
+                position.height.value = height;
+                position.width.value = width;
+                position.ready.value = true;
+
+                runOnUI(() => {
+                  "worklet";
+                  if (positions.every((pos) => pos.ready.value === true)) {
+                    runOnJS(setReady)(true);
+                  }
+                })();
+              }}
+            >
+              <SortableTask
+                title={task.title}
+              />
+            </View>
+          );
+        })}
+      </ScrollView>
+    );
+  }
 
   return (
     <ScrollView>
       {sortableTasks.map((t) => {
         return (
-          <SortableTask key={t.id} title={t.title} />
+          <SortableTask
+            key={t.id}
+            title={t.title}
+          />
         );
       })}
     </ScrollView>
