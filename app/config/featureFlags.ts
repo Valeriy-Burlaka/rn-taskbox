e5e6a42892c61be46dc899ede93b6cda6a06b337
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2023-05-17 19:07:56 +0200

    document feature flag meaning

--- app/config/featureFlags.ts
+++ app/config/featureFlags.ts
@@ -1,1 +1,3 @@
+// Adds a new menu item - "Sort Tasks" - to the context menu in the task list.
+// Also, disables the ability to "Pin" tasks, as this sorting mechanism ("Pinned" tasks sorted on top) becomes irrelevant.
 export const SORT_TASKS_FEATURE_ENABLED = true;
